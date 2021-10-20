6845b4d407d6b8f3df4a38a5d41dcdaa377fde96
IShahnawazShaikh <shahnawazexplore@gmail.com>
2021-10-20 16:41:54 +0530

    First-App

---
+++ learning-app/src/Components/FunctionStudentComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+const FunctionStudentComponent=(props)=>{
+
+   return <h1>Hello {props.name} pursuing {props.course}</h1>
+
+}
+
+export default FunctionStudentComponent;
---
+++ learning-app/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import FunctionStudentComponent from "./Components/FunctionStudentComponent";
+
+ReactDOM.render(<FunctionStudentComponent name="shahnawaz" course="CSE"/>,document.getElementById("root"));
