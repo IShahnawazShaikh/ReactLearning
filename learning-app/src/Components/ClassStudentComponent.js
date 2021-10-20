554fb1840ac482ce534449b8c73912404b9249e4
IShahnawazShaikh <shahnawazexplore@gmail.com>
2021-10-20 16:49:44 +0530

    Class Component Updated

---
+++ learning-app/src/Components/ClassStudentComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+
+class ClassStudentComponent extends Component{
+
+  render(){
+
+    return <h1>Hello {this.props.name}</h1>
+  }
+
+
+}
+export default ClassStudentComponent;
--- learning-app/src/index.js
+++ learning-app/src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ClassStudentComponent from "./Components/ClassStudentComponent";
 import FunctionStudentComponent from "./Components/FunctionStudentComponent";
 
-ReactDOM.render(<FunctionStudentComponent name="shahnawaz Shaikh" course="CSE"/>,document.getElementById("root"));
+
+//ReactDOM.render(<FunctionStudentComponent name="shahnawaz Shaikh" course="CSE"/>,document.getElementById("root"));
+ReactDOM.render(<ClassStudentComponent name="shahnawaz" course="CSE"/>,document.getElementById("root"));
